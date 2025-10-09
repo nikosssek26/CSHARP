962e0f12c58906d64ccb7e8b131ffcc4a5441c31
nikosssek <nikodemzsp3zamosc@gmail.com>
2025-10-09 10:17:08 +0200

    nowe zadanie CS2

---
+++ CS2/CS2/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace CS2
+{
+    class Tablica
+    {
+        public int[] tab = new int[3];
+        public Tablica(int[] tab)
+        {
+            this.tab = tab;
+        }
+        public Tablica()
+        {
+            tab= [0,0,0,];
+        }
+        public Tablica(Tablica tab)
+        {
+            //this.tab = tab.tab;
+            for (int i=0; i<tab.tab.Length; i++)
+                this.tab[i] = tab.tab[i];
+        }
+
+        public override string ToString()
+        {
+            string s ="";
+            foreach (int i in tab)
+                s+= i.ToString() + " ";
+                return s;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tablica t1 = new Tablica();
+            Tablica t2 = new Tablica(new int[3] {1,2,3});
+            Tablica t3 = new Tablica(t2);
+            t3.tab[0] = 100;
+            System.Console.WriteLine(t1);
+            System.Console.WriteLine(t2);
+            System.Console.WriteLine(t3);
+        }
+    }
+}
