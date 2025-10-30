4d42dbbf005e0f02be09af6a1c1b0feca73047d0
nikosssek <nikodemzsp3zamosc@gmail.com>
2025-10-30 10:15:42 +0100

    program pralka na ocene 30.10.2025 + CS3

---
+++ CS3 pojazdy/CS3/Program.cs
@@ -0,0 +1,85 @@
+﻿namespace CS3
+{
+    abstract class SrodekTransportu
+    {
+        public abstract string nazwa { get; protected set; }
+    }
+    interface Ijedz
+    {
+        void jedz();
+    }
+    interface IPlyn
+    {
+        void plyn();
+    }
+    interface ILataj
+    {
+        void lec();
+    }
+
+    abstract class Ladowy : SrodekTransportu, Ijedz 
+    {
+        public abstract void jedz();
+    }
+    abstract class Wodne : SrodekTransportu, IPlyn
+    {
+        public abstract void plyn();
+
+    }
+    abstract class Powietrzne : SrodekTransportu, ILataj 
+    {
+        public abstract void lec();
+    }
+
+    class Samochod : Ladowy
+    {
+        public override string nazwa { get; protected set; }
+        public Samochod(string nazwa) => this.nazwa = nazwa;
+        public override void jedz() 
+        {
+            Console.WriteLine($"{nazwa} jedzie");
+        }
+    }
+    class Kajak : Wodne 
+    {
+        public override string nazwa { get; protected set; }
+        public Kajak(string nazwa) => this.nazwa = nazwa;
+        public override void plyn()
+        {
+            Console.WriteLine($"{nazwa} plynie");
+        }
+    }
+    class Samolot : Powietrzne
+    {
+        public override string nazwa { get; protected set; }
+        public Samolot(string nazwa) => this.nazwa = nazwa;
+        public override void lec()
+        {
+            Console.WriteLine($"{nazwa} lata");
+        }
+    }
+    class Amfibia : Ladowy, IPlyn
+    {
+        public override string nazwa { get; protected set; }
+        public Amfibia(string nazwa) => this.nazwa = nazwa;
+        public override void jedz()
+        {
+            Console.WriteLine($"{nazwa} jedzie");
+        }
+        public void plyn()
+        {
+            Console.WriteLine($"{nazwa} plynie");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Samochod samochod = new Samochod("Mercedes-Benz AffalterBach C63 W204");
+            Kajak kajak = new Kajak("Drewniany kajak");
+            Samolot samolot = new Samolot("Cessna 172");
+            Amfibia amfibia = new Amfibia("Gibbs Aquada");
+        }
+    }
+}
---
+++ CSP4 pralka/CSP4/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace CSP4
+{
+    class Pralka
+    {
+        private int program;
+        private int wirowanie;
+
+        public Pralka() { program = 1; wirowanie = 100; }
+
+        public void PlusWirowanie()
+        {
+            Console.WriteLine("Poziom Wirowania wzniesiony o 100!");
+            wirowanie += 100;
+            if (wirowanie > 1000)
+            {
+                Console.WriteLine("Za wysokie wirowanie! Ustawiam Wartość domyślną");
+                wirowanie = 100;
+            }
+        }
+        public void MinusWirowanie()
+        {
+            Console.WriteLine("Poziom Wirowania zniżony o 100!");
+            wirowanie -= 100;
+            if (wirowanie < 100)
+            {
+                Console.WriteLine("Za niskie wirowanie! Ustawiam Wartość domyślną");
+                wirowanie = 100;
+            }
+        }
+        public void PlusProgram()
+        {
+            Console.WriteLine("Program pralki wzniesiony o 1!");
+            program += 1;
+            if (program > 5)
+            {
+                Console.WriteLine("Program 6 w tej pralce nie istnieje!!");
+                program = 5;
+            }
+        }
+        public void MinusProgram()
+        {
+            Console.WriteLine("Program pralki zniżony o 1!");
+            program -= 1;
+            if (program < 1)
+            {
+                Console.WriteLine("Nie można zniżyć programu do zera");
+                program = 1;
+            }
+        }
+        public void Uruchom()
+        {
+            Console.WriteLine($"Pralka uruchomiona z programem nr.{program} oraz wirowaniem na poziomie: {wirowanie}"); 
+        }
+    }
+
+    internal class Program()
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" 1 - codzienne \n 2 - delikatne \n 3 - jeans \n 4 - bawelna  \n 5 - eco \n \n");
+            Console.WriteLine("Pierwsza pralka: \n");
+            Pralka pralka = new Pralka();
+            pralka.PlusProgram();
+            pralka.PlusProgram();
+            pralka.PlusProgram();
+
+            pralka.PlusWirowanie();
+            pralka.PlusWirowanie();
+            pralka.PlusWirowanie();
+
+            pralka.Uruchom();
+
+            Pralka pralka2 = new Pralka();
+            Console.WriteLine("\n Druga pralka: \n");
+            pralka2.MinusProgram();
+            pralka2.PlusProgram();
+            pralka2.PlusProgram();
+
+            pralka2.MinusWirowanie();
+            pralka2.PlusWirowanie(); 
+            pralka2.Uruchom();
+        }
+    }
+}
