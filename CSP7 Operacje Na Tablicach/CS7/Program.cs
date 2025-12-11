0207cd105d450c15270072ecbbfa066faf1f1984
nikosssek <nikodemzsp3zamosc@gmail.com>
2025-12-11 10:06:56 +0100

    11.12.2025

---
+++ CSP7 Operacje Na Tablicach/CS7/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace CS7
+{
+    class Tablica
+    {
+        int[] tablicaZ;
+        int rozmiar;
+        public Tablica(int rozmiar)
+        {
+            this.rozmiar = rozmiar;
+            tablicaZ = new int[rozmiar];
+            for (int i = 0; i < rozmiar; i++)
+            {
+                Random rand = new Random();
+                tablicaZ[i] = rand.Next(1, 1000);
+            }
+        }
+        public void WyswietlTablica()
+        {
+            System.Console.Write("Wszystkie elementy tablicy: \n");
+            for (int i = 0; i < rozmiar; i++)
+            {
+                System.Console.Write("nr: "+tablicaZ[i] + " \n");
+            }
+            System.Console.WriteLine("\n");
+        }
+        public void WyszukajElement(int element)
+        {
+            for (int i = 0; i < rozmiar; i++)
+            {
+                if (tablicaZ[i] == element)
+                {
+                    System.Console.WriteLine("Znalazlem element: " + element + " na pozycji: " + i);
+                    return;
+                }
+            }
+        }
+        public void WyswietlParzyste()
+        {
+            int count = 0;
+            System.Console.Write("Parzyste elementy tablicy: \n");
+            for (int i = 0; i < rozmiar; i++)
+            {
+                if (tablicaZ[i] % 2 == 0)
+                {
+                    System.Console.Write("nr: " + tablicaZ[i] + " \n");
+                    count++;
+                }
+            }
+            System.Console.WriteLine("Liczba parzystych elementow: " + count);
+            System.Console.WriteLine();
+        }
+        public void SredniaArtymetyczna()
+        {
+            int suma = 0;
+            for (int i = 0; i < rozmiar; i++)
+            {
+                suma += tablicaZ[i];
+            }
+            double srednia = (double)suma / rozmiar;
+            System.Console.WriteLine("Srednia arytmetyczna elementow tablicy: " + srednia);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tablica T1 = new Tablica(100);
+            T1.WyswietlTablica();
+            T1.WyszukajElement(732);
+            T1.WyswietlParzyste();
+            T1.SredniaArtymetyczna();
+        }
+    }
+}
