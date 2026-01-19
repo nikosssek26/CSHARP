93b24507b23c35ed2f25ea8548ab03de9086a8aa
Nikodem <ut_4236@zsp3zamosc.pl>
2026-01-19 12:10:54 +0100

    pon. 19.01.2026

---
+++ CODEWARS/CDW13/CDW13/Program.cs
@@ -0,0 +1,7 @@
+﻿public class Kata{
+    public static int Grow(int[] x){
+        int product = 1;
+        for (int i = 0; i < x.Length; i++) product *= x[i];
+        return product;
+    }
+}
---
+++ CODEWARS/CDW14/CDW14/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Kata
+{
+    public static string removeBMW(string text)
+    {
+        if(text == string.Empty && text == null) return null;
+        foreach (var item in text)
+        {
+            if(item == 'B' || item == 'M' || item == 'W' || item == 'b' || item == 'w' || item == 'm')
+            {
+                text = text.Replace(item.ToString(), "");
+            }
+        }
+        return text;
+    }
+}
+public class Program
+{
+    public static void Main()
+    {
+        string text = "This is a BMW car. bmw is popular.";
+        string result = Kata.removeBMW(text);
+        System.Console.WriteLine(result);
+    }
+}
